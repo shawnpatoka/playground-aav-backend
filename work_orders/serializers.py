acd72474f4dabfefcc7dfbfba7d7e91fbb4e1990
Shawn Patoka <76219517+shawnpatoka@users.noreply.github.com>
2023-04-04 15:37:16 -0500

    added fields to serializers

--- work_orders/serializers.py
+++ work_orders/serializers.py
@@ -3,15 +3,17 @@ from .models import WorkOrder
 from users.serializers import UserSerializer
 
 class WorkOrderSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer()
+    
     class Meta:
         model = WorkOrder
-        fields = '__all__'
+        fields = ['id', 'site','date', 'status', 'arrival', 'est_completion', 'assigned_to', 'description', 'parts_used', 'work_completed', 'work_to_be_completed','work_time_start','work_time_end']
 
 
 class WorkOrderListSerializer(serializers.ModelSerializer):
 
     assigned_to = UserSerializer()
-    
+
     class Meta:
         model = WorkOrder
         fields = ['id', 'site', 'status', 'arrival', 'est_completion', 'assigned_to']
